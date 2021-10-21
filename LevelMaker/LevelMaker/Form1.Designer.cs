c0ba28a71ace1f3681ab28ba797977d835b57d01
ShadowAshyn <shadow9102001@gmail.com>
2021-10-20 20:30:36 -0400

    External Tool Addition
    Level Builder Tool V1

---
+++ LevelMaker/LevelMaker/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace LevelMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shapeCheckBox = new System.Windows.Forms.CheckedListBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.heightBox = new System.Windows.Forms.TextBox();
+            this.widthBox = new System.Windows.Forms.TextBox();
+            this.widthLabel = new System.Windows.Forms.Label();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.previewLabel = new System.Windows.Forms.Label();
+            this.previewBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // shapeCheckBox
+            // 
+            this.shapeCheckBox.FormattingEnabled = true;
+            this.shapeCheckBox.Items.AddRange(new object[] {
+            "Square(Large)",
+            "Square(Small)",
+            "Triangle(Large)",
+            "Triangle(Medium)",
+            "Triangle(Small)",
+            "Parallelogram",
+            "Rectangle(Short)",
+            "Rectangle(Medium)",
+            "Rectangle(Long)",
+            "L-Block",
+            "J-Block",
+            "T-Block",
+            "S-Block",
+            "Z-Block"});
+            this.shapeCheckBox.Location = new System.Drawing.Point(12, 100);
+            this.shapeCheckBox.Name = "shapeCheckBox";
+            this.shapeCheckBox.Size = new System.Drawing.Size(194, 214);
+            this.shapeCheckBox.TabIndex = 0;
+            this.shapeCheckBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.shapeCheckBox_ItemCheck);
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(12, 337);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(194, 48);
+            this.generateButton.TabIndex = 1;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // heightBox
+            // 
+            this.heightBox.Location = new System.Drawing.Point(12, 44);
+            this.heightBox.Name = "heightBox";
+            this.heightBox.Size = new System.Drawing.Size(95, 20);
+            this.heightBox.TabIndex = 3;
+            this.heightBox.TextChanged += new System.EventHandler(this.heightBox_TextChanged);
+            // 
+            // widthBox
+            // 
+            this.widthBox.Location = new System.Drawing.Point(111, 44);
+            this.widthBox.MaxLength = 3200;
+            this.widthBox.Name = "widthBox";
+            this.widthBox.Size = new System.Drawing.Size(95, 20);
+            this.widthBox.TabIndex = 5;
+            this.widthBox.TextChanged += new System.EventHandler(this.widthBox_TextChanged);
+            // 
+            // widthLabel
+            // 
+            this.widthLabel.Location = new System.Drawing.Point(111, 13);
+            this.widthLabel.Name = "widthLabel";
+            this.widthLabel.Size = new System.Drawing.Size(95, 13);
+            this.widthLabel.TabIndex = 4;
+            this.widthLabel.Text = "Width";
+            this.widthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.Location = new System.Drawing.Point(12, 13);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(95, 13);
+            this.heightLabel.TabIndex = 6;
+            this.heightLabel.Text = "Height";
+            this.heightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // previewLabel
+            // 
+            this.previewLabel.Location = new System.Drawing.Point(215, 68);
+            this.previewLabel.Name = "previewLabel";
+            this.previewLabel.Size = new System.Drawing.Size(264, 21);
+            this.previewLabel.TabIndex = 8;
+            this.previewLabel.Text = "Preview";
+            this.previewLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // previewBox
+            // 
+            this.previewBox.Location = new System.Drawing.Point(218, 100);
+            this.previewBox.Multiline = true;
+            this.previewBox.Name = "previewBox";
+            this.previewBox.Size = new System.Drawing.Size(261, 214);
+            this.previewBox.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 404);
+            this.Controls.Add(this.previewBox);
+            this.Controls.Add(this.previewLabel);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.widthBox);
+            this.Controls.Add(this.widthLabel);
+            this.Controls.Add(this.heightBox);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.shapeCheckBox);
+            this.Name = "Form1";
+            this.Text = "Level Maker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox shapeCheckBox;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.TextBox heightBox;
+        private System.Windows.Forms.TextBox widthBox;
+        private System.Windows.Forms.Label widthLabel;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.Label previewLabel;
+        private System.Windows.Forms.TextBox previewBox;
+    }
+}
+
---
+++ LevelMaker/LevelMaker/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LevelMaker
+{
+    /// <summary>
+    /// Created by: Dylan Mahala
+    /// Created on: 10/20/21
+    /// Purpose: Level Designer for Tangram Trouble
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        //Notes:
+        //Square(Large) : SL
+        //Square(Small) : SS
+        //Triangle(Large) : TL
+        //Triangle(Medium) : TM
+        //Triangle(Small) : TS
+        //Parallelogram : P
+        //Rectangle(Short) : RS
+        //Rectangle(Medium) : RM
+        //Rectangle(Long) : RL
+        //L-Block : L
+        //J-Block : J
+        //T-Block : T
+        //S-Block : S
+        //Z-Block : Z
+
+        StreamWriter sw = File.CreateText("../../../Level.txt");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Generates the final file
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            sw.WriteLine(previewBox.Text);
+            sw.Close();
+        }
+
+        //Adding the shapes using the checked and unchecked boxes
+        private void shapeCheckBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            switch (e.Index)
+            {
+                //Large Square
+                case 0:
+                    previewBox.Text += " LS ";
+                    break;
+                //Small Square
+                case 1:
+                    previewBox.Text += " SS ";
+                    break;
+                //Large Triangle
+                case 2:
+                    previewBox.Text += " TL ";
+                    break;
+                //Medium Triangle
+                case 3:
+                    previewBox.Text += " TM ";
+                    break;
+                //Small Triangle
+                case 4:
+                    previewBox.Text += " TS ";
+                    break;
+                //Parallelogram
+                case 5:
+                    previewBox.Text += " P ";
+                    break;
+                //Short Rectangle
+                case 6:
+                    previewBox.Text += " RS ";
+                    break;
+                //Medium Rectangle
+                case 7:
+                    previewBox.Text += " RM ";
+                    break;
+                //Long Rectangle
+                case 8:
+                    previewBox.Text += " RL ";
+                    break;
+                //L-Block
+                case 9:
+                    previewBox.Text += " L ";
+                    break;
+                //J-Block
+                case 10:
+                    previewBox.Text += " J ";
+                    break;
+                //T-Block
+                case 11:
+                    previewBox.Text += " T ";
+                    break;
+                //S-Block
+                case 12:
+                    previewBox.Text += " S ";
+                    break;
+                //Z-Block
+                case 13:
+                    previewBox.Text += " Z ";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void heightBox_TextChanged(object sender, EventArgs e)
+        {
+            HeightWidth();
+        }
+
+        private void widthBox_TextChanged(object sender, EventArgs e)
+        {
+            HeightWidth();
+        }
+
+        void HeightWidth()
+        {
+            if (heightBox.Text != "" && widthBox.Text != "")
+            {
+                try
+                {
+                    int height = Int32.Parse(heightBox.Text);
+                    int width = Int32.Parse(widthBox.Text);
+
+                    previewBox.AppendText(Environment.NewLine + height + " " + width);
+                }
+                catch (Exception)
+                {
+                    previewBox.AppendText(Environment.NewLine + "Error in Parse");
+                }
+            }
+            
+        }
+    }
+}
